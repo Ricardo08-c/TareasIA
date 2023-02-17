8b4d8adbacf34d14a0af1d1ec58453d8bc7d59ec
Rircardo08-c <ricardosora08@gmail.com>
2023-02-16 18:43:10 -0600

    Cmbio pos

--- Tarea1/Menu-basico.rkt
+++ Tarea1/Menu-basico.rkt
@@ -1,14 +1,15 @@
 #lang racket
- (define my-player (player "Alice" '(move1 move2) 10))
+(struct player (symbol moves moveQuant)#:mutable) 
 
 (define (display-menu)
   (displayln "Menu:")
-  (displayln "1. Option 1")
-  (displayln "2. Option 2")
-  (displayln "3. Option 3")
-  (displayln "4. Exit"))
-
+  (displayln "1. Llenar manualmente")
+  (displayln "2. Llenar aleatoriamente")
+  (displayln "4. Salir")
+)
 (define (get-user-choice)
+  
+  
   (display "Enter your choice: ")
   (flush-output)
   (define input (read-line))
@@ -21,19 +22,93 @@
      (displayln "Invalid choice, please try again.")
      (get-user-choice)]))
 
+(define (displayMat matrix i j)
+    (if(>= i (length matrix))
+       (display #\newline )
+       (if(< j(length (list-ref matrix i)))
+       (begin
+          (display (list-ref(list-ref matrix i) j))
+         (display #\ )
+         (display #\ )
+         (display #\| )
+         
+         (display #\ )
+         (display #\ )
+         (displayMat matrix i (+ j 1)))
 
-
+       (begin
+         
+         (display #\newline )
+         (display #\| )
+         
+         
+         (displayMat matrix (+ i 1) 0))
+       
+        
+       
+       ))
+      
+     
+       )
+         
+      
+ 
   
+(define (movePieces p1 p2 matriz)
+        
+         
+         
+         
+         
+         
+         (display "Ingresa el movimiento, ejemplo : 1 2 ")
+  
+  
+  
+  
+  (flush-output)
+  (define input (read-line))
+  (define fila(-(char->integer(string-ref input 0)) 48))
+  (define col(-(char->integer(string-ref input 2)) 48))
+  
+  
+  (define volcado(list-set (list-ref matriz fila) col "X"))
+
+  (list-set matriz fila volcado)
+  
+  
+  
+  
+  
+        
+        
+         )
+
+
+
+(define matriz(build-list 6 (lambda (i)
+                 (build-list 5 (lambda (j) 0)))))
+
+
+(define (playerTurnMenu player1 player2)
+  (set! matriz (movePieces player1 player2 matriz))
+       (display #\| )
+       (displayMat matriz 0 0)
+  (playerTurnMenu player1 player2)
+  )
 (define (run-menu)
 
-
+  (define player1 (player "X" list 0))
+  (define player2 (player "O" list 0))
   (define (loop)
     (displayln "Welcome to the menu!")
     (display-menu)
     (define choice (get-user-choice))
     (cond
       [(eq? choice 'option1)
-       (displayln "You chose Option 1.")
+       ;(fillAuto)
+       
+       (playerTurnMenu player1 player2)
        (loop)]
       [(eq? choice 'option2)
        (displayln "You chose Option 2.")
