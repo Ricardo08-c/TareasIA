eb0b7adba326ed6ece1c71eaee2f0320905852b2
Rircardo08-c <ricardosora08@gmail.com>
2023-02-19 16:20:04 -0600

    fffff

--- Tarea1/Menu-basico.rkt
+++ Tarea1/Menu-basico.rkt
@@ -1,5 +1,4 @@
 #lang racket
-(require simple-matrix)
 
 (struct player (symbol moves moveQuant)#:mutable) 
 (define matriz(build-list 6 (lambda (i)
@@ -64,7 +63,7 @@
          
          
          
-  (display "Ingresa el movimiento, ejemplo : 1 2 ")
+  (display "Ingresa la ficha, ejemplo : 1 2  ")
   
   
   
@@ -94,7 +93,7 @@
 
 (define(countFilledSpaces i j count)
   (if(>= i (length matriz))
-       (>= count 5)
+       (>= count 24)
        (if(< j(length (list-ref matriz i)))
        (begin
           (if(not(equal? (list-ref(list-ref matriz i) j) '#\ ))
@@ -118,9 +117,148 @@
        ))
   )
 
-(define(displayPlayMenu)
-  (0)
+(define (cogerFichaEnemiga currPlayer)
+    (displayln #\ )
+  
+               
+    (displayln "¡Lograste alinear 3 elementos, coje una ficha enemiga! Ingresa la ficha, ejemplo : 1 2 ")
+  
+  (flush-output)
+  (define input (read-line))
+  (define fila(-(char->integer(string-ref input 0)) 48))
+  (define col(-(char->integer(string-ref input 2)) 48))
+  
+  ; validar que la ficha sea la contraria a currPlayer
+  
+  (define volcado(list-set (list-ref matriz fila) col #\ ))
+  
+
+  (list-set matriz fila volcado)
+
+  
+  
   )
+(define(displayPlayMenu player1 player2 currPlayer)
+    
+ 
+  (displayln (string-append "Turno de: " (player-symbol currPlayer))  )
+  (define cnd(movePiecesGame  currPlayer))
+  
+  (if(equal? cnd #t)
+     (if(equal? currPlayer player1)
+     (set! currPlayer player2)
+     (set! currPlayer player1)
+     )
+     (set! currPlayer currPlayer)
+     
+     
+     
+     
+     )
+  (if(> 3  2)
+     (cogerFichaEnemiga currPlayer)
+     #\
+     )
+  (display #\| )
+  
+       (displayMat matriz 0 0)
+  
+  (displayPlayMenu  player1 player2 currPlayer)
+  )
+
+
+(define(moveIsValid rowIn colIn rowDes colDes currPlayer )
+   (define cond1(equal?(player-symbol currPlayer) (list-ref(list-ref matriz rowIn) colIn)))
+  (define cond2(equal?(list-ref(list-ref matriz rowDes) colDes) #\ ))
+  (define rowMove(and(= (abs (- rowIn rowDes)) 1) (= colIn colDes)))
+  (define colMove(and(= (abs (- colIn colDes)) 1) (= rowIn rowDes)))
+  
+
+
+  
+  (and cond1 cond2 (or rowMove colMove))
+ 
+     
+  
+  )
+
+
+
+
+(define (movePiecesGame  currPlayer)
+        
+         
+         
+         
+         
+         
+  (display "Ingresa el movimiento, debe respetar los espacios, sintáxis: $/1 2 -> 3 2 ")
+  
+  
+  
+  
+  (flush-output)
+  (define input (read-line))
+  (define filaIn(-(char->integer(string-ref input 0)) 48))
+  (define colIn(-(char->integer(string-ref input 2)) 48))
+  (define filaDes(-(char->integer(string-ref input 7)) 48))
+  (define colDes(-(char->integer(string-ref input 9)) 48))
+
+  
+  
+  (define simbolo(player-symbol currPlayer))
+  
+  (define volcado(list-set (list-ref matriz filaDes) colDes (list-ref(list-ref matriz filaIn) colIn)))
+  (define cond (moveIsValid filaIn colIn filaDes colDes currPlayer))
+  (if cond      
+      
+      (set! matriz(list-set matriz filaDes volcado))
+           
+      #\
+      )
+  (define anterior(list-set (list-ref matriz filaIn) colIn #\ ))
+  
+  
+  
+  ; Aqui se debe validar el movimiento (que no hayan fichas donde lo mueve, que haga un 3 en línea
+  
+  (if cond
+      (begin
+        (set! matriz(list-set matriz filaIn anterior))
+        #t
+      
+      
+      
+      
+      
+      )
+      
+      (begin
+        (writeln #\ )
+        (writeln "XXXXXXXXXXXXXX" )
+            (writeln "Movimiento inválido")
+            (writeln "XXXXXXXXXXXXXX" )
+            (writeln #\ )
+            #f
+            
+            )
+      )
+      
+      
+
+ 
+  
+  
+  
+  
+  
+        
+        
+         )
+
+
+
+
 (define (playerTurnMenu player1 player2 currPlayer)
   
   (if(equal? currPlayer player1)
@@ -136,55 +274,69 @@
        (displayMat matriz 0 0)
   
   (if (countFilledSpaces 0 0 0)
-(displayPlayMenu)
+      (begin
+        (writeln "Inicio del juego!")
+(displayPlayMenu currPlayer player1 player2)
+
+)
 (playerTurnMenu player1 player2 currPlayer)
       )
   
   
   )
-
-
-
-
+(define (formaLinea fila columna caracter)
+  #t
+  )
 (define (randomRowNumber)
-  (random 5))
-
-(define (randomColumnNumber)
   (random 6))
 
-(define (randomMovement matriz currPlayer) 
- (define row (randomRowNumber))
- (define column (randomColumnNumber))
- (displayln row)
- (displayln column)
- (define volcado(list-set (list-ref matriz row) column (player-symbol currPlayer)))
-  
+(define (randomColumnNumber)
+  (random 5))
 
-  (list-set matriz row volcado)
+(define (randomBoard player1 player2 currPlayer)
+   (define row (randomRowNumber))
+   (define column (randomColumnNumber))
+  (define simbolo(player-symbol currPlayer))
+  
+  (display #\|)
+  (displayMat matriz 0 0)
+  (define anterior(list-set (list-ref matriz row) column simbolo))
+  (if (not(countFilledSpaces 0 0 0))
+      (begin
+        (displayln "Se rellenó el tablero aleatoriamente")
+      (if(not(formaLinea row column simbolo))
+         (begin
+              (set! matriz(list-set matriz row anterior))
+               (if(equal? currPlayer player1)
+                  (set! currPlayer player2)
+                  (set! currPlayer player1)
+                  )
+               (randomBoard player1 player2 currPlayer)
+               )
+         
+         (randomBoard player1 player2 currPlayer)
+         
+         )
+        
+        
+       
 
 )
-
-(define (randomBoard player1 player2 currPlayer) 
-
-  (if(equal? currPlayer player1)
-     (set! currPlayer player2)
-     (set! currPlayer player1)
-     )
- (displayln (string-append "Turno de: " (player-symbol currPlayer))  )
- (set! matriz (randomMovement matriz currPlayer))
-
-  (display #\| )
-  
-       (displayMat matriz 0 0)
-  
-  (if (countFilledSpaces 0 0 0)
-(displayPlayMenu)
-(randomBoard player1 player2 currPlayer)
+#\
       )
- (displayln "Se rellenó el tablero aleatoriamente")
+ 
 
   
+
+
+  
+ (displayln row)
+ (displayln column)
+
  )
+
+(define (transpose matrix)
+  (apply map list matrix))
 
 
 (define (no-hay-3-repeticiones? matriz)
@@ -210,6 +362,7 @@
         (else (existen-3-repeticiones-aux? fila (+ n 1)))))
 
 
+
 (define (run-menu)
 
   (define player1 (player "X" list 0))
@@ -221,7 +374,7 @@
     (define choice (get-user-choice))
     (cond
       [(eq? choice 'option1)
-       ;(fillAuto)
+       
        
        (playerTurnMenu player1 player2 currPlayer)
        (loop)]
